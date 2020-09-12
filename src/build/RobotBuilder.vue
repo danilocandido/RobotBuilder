0a832c804e1df05a31f2495d50d4920fe91d916e
Danilo Cândido <danilod07@gmail.com>
2020-09-11 21:59:09 -0300

    Add cart element

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
+  <div class="content">
+    <button class="add-to-cart" @click="addToCard()">Add to Card</button>
     <div class="top-row" >
       <div class="top part">
+        <div class="robot-name">
+          {{ selectedRobot.head.title }}
+          <span :v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
+        </div>
         <img :src="selectedRobot.head.src" title="head"/>
         <button @click="selectPreviousHead()" class="prev-selector">&#9668;</button>
         <button @click="selectNextHead()" class="next-selector">&#9658;</button>
@@ -31,6 +36,23 @@
         <button @click="selectNextBase()" class="next-selector">&#9658;</button>
       </div>
     </div>
+    <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot, index) in cart" :key="index">
+            <td>{{robot.head.title}}</td>
+            <td class="cost">{{robot.head.cost}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -51,6 +73,7 @@ export default {
   name: 'RobotBuilder',
   data() {
     return {
+      cart: [],
       availableParts,
       selectHeadIndex: 0,
       selectLeftArmIndex: 0,
@@ -71,6 +94,16 @@ export default {
     },
   },
   methods: {
+    addToCard() {
+      const robot = this.selectedRobot;
+      const cost = robot.head.cost
+        + robot.leftArm.cost
+        + robot.rightArm.cost
+        + robot.torso.cost
+        + robot.base.cost;
+
+      this.cart.push({ ...robot, cost });
+    },
     selectNextHead() {
       this.selectHeadIndex = getNextValidIndex(this.selectHeadIndex,
         availableParts.heads.length);
@@ -203,5 +236,32 @@ export default {
 }
 .right .next-selector {
   right: -3px;
+}
+.robot-name {
+  position: absolute;
+  top: -25px;
+  text-align: center;
+  width: 100%;
+}
+.sale {
+  color: red;
+}
+.content {
+  position: relative;
+}
+.add-to-cart {
+  position: absolute;
+  right: 30px;
+  width: 220px;
+  padding: 3px;
+  font-size: 16px;
+}
+td, th {
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
+}
+.cost {
+  text-align: right;
 }
 </style>
